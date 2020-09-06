77e3a9e2ee6f5d3af637235bd34628cfa34c43fd
Mumi <543528285@qq.com>
2020-09-06 15:40:33 +0800

    修复两个bug

--- src/components/goods/GoodsParams.vue
+++ src/components/goods/GoodsParams.vue
@@ -11,7 +11,6 @@
       <el-row>
         <el-col>
           <span>选择商品分类：</span>
-
           <el-cascader
             v-model="selctedCateKeys"
             :options="cateList"
@@ -31,7 +30,22 @@
           <el-button type="primary" size="mini" :disabled="isButtondisabled" @click="showAddDialogVisible">添加参数</el-button>
           <el-table :data="manyTableData" border style="width: 100%">
             <!-- 展开行 -->
-            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="expand">
+              <template slot-scope="scope">
+                <el-tag type="danger" closable v-for="(item,index) in scope.row.attr_vals" :key="index" @close="handleClose(index,scope.row)">{{item}}</el-tag>
+                   <el-input
+                      class="input-new-tag"
+                      v-if="inputVisible"
+                      v-model="inputValue"
+                      ref="saveTagInput"
+                      size="small"
+                      @keyup.enter.native="handleInputConfirm(scope.row)"
+                      @blur="handleInputConfirm(scope.row)"
+                    >
+                  </el-input>
+                  <el-button v-else class="button-new-tag" size="small" @click="showInput(scope.row)">+ New Tag</el-button>
+              </template>
+            </el-table-column>
             <el-table-column type="index" label="#"></el-table-column>
             <el-table-column label="属性名称" prop="attr_name"></el-table-column>
             <el-table-column label="操作" width="180px">
@@ -107,6 +121,9 @@ export default {
       manyTableData: [],
       onlyTableData: [],
       addDialogVisible: false,
+      // input框的显示和隐藏
+      inputVisible: false,
+      inputValue: '',
       // 添加参数的表单数据对象
       addForm: {
         attr_name: ''
@@ -140,15 +157,27 @@ export default {
       // console.log(this.cateList)
     },
     async handleChange () {
+      // 修复了一个只选中2级菜单 就会报错的问题 **
+      if (this.selctedCateKeys.length !== 3) {
+        this.selctedCateKeys = []
+        this.manyTableData = []
+        this.onlyTableData = []
+        return
+      }
       // console.log(this.selctedCateKeys)
       const { data: res } = await this.$http.get(`categories/${this.cateId}/attributes`, { params: { sel: this.activeName } })
       if (this.activeName === 'many') {
+        // 遍历循环res.data 让其中的attr_vals转换成数组 以便渲染到页面上
+        res.data.forEach(item => {
+          item.attr_vals = item.attr_vals ? item.attr_vals.split(' ') : []
+        })
         this.manyTableData = res.data
+        // console.log('many', this.manyTableData)
       } else {
         this.onlyTableData = res.data
       }
 
-      console.log(res)
+      // console.log(res)
     },
     handleTabsClick () {
       // console.log(11)
@@ -167,7 +196,7 @@ export default {
       this.$refs.addFormRef.validate(async valid => {
         if (!valid) return
         const { data: res } = await this.$http.post(`categories/${this.cateId}/attributes`, { attr_name: this.addForm.attr_name, attr_sel: this.activeName })
-        console.log(res)
+        // console.log(res)
         if (res.meta.status !== 201) return this.$message.error('添加' + this.titleText + '失败')
         this.$message.success('添加' + this.titleText + '成功')
         this.addDialogVisible = false
@@ -192,7 +221,7 @@ export default {
       this.$refs.editFormRef.validate(async valid => {
         if (!valid) return
         const { data: res } = await this.$http.put(`categories/${this.cateId}/attributes/${this.editForm.attr_id}`, { attr_name: this.editForm.attr_name, attr_sel: this.activeName })
-        console.log(res)
+        // console.log(res)
         if (res.meta.status !== 200) return this.$message.error('编辑' + this.titleText + '失败')
         this.$message.success('编辑' + this.titleText + '成功')
         this.editDialogVisible = false
@@ -211,7 +240,50 @@ export default {
       if (res.meta.status !== 200) return this.$message.error('删除参数数据失败')
       this.$message.success('删除参数成功')
       this.handleChange()
+    },
+    // 添加动态参数
+    handleInputConfirm (row) {
+      // 先判断用户输入的数据是否合法
+      console.log(this.inputValue)
+      if (this.inputValue.trim().length === 0) {
+        this.inputValue = ''
+        this.inputVisible = false
+        return
+      }
+      // this.inputVisible = false
+      // 如果用户输入了真实的数据 保存起来
+      row.attr_vals.push(this.inputValue.trim())
+      // 解决了发送两次请求的问题****
+      if (this.inputValue !== '') {
+        this.inputValue = ''
+        this.inputVisible = false
+        this.saveAttrVals(row)
+      }
+    },
+
+    // 召唤出输入框
+    showInput (row) {
+      // 这个显示只能在目标的焦点选中之前
+      this.inputVisible = true
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus()
+        // this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
+    // 关闭tag框删除动态参数
+    handleClose (index, row) {
+      row.attr_vals.splice(index, 1)
+      this.saveAttrVals(row)
+    },
+
+    // 封装一个保存可选项的操作的函数
+    async saveAttrVals (row) {
+      const { data: res } = await this.$http.put(`categories/${this.cateId}/attributes/${row.attr_id}`, { attr_name: row.attr_name, attr_sel: row.attr_sel, attr_vals: row.attr_vals.join(' ') })
+      if (res.meta.status !== 200) return this.$message.error('修改失败')
+      this.$message.success('修改成功')
+      // this.handleChange()
     }
+
   },
   computed: {
     isButtondisabled () {
@@ -235,4 +307,10 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.el-tag{
+  margin-right: 20px;
+}
+.input-new-tag{
+  width: 90px;
+}
 </style>
